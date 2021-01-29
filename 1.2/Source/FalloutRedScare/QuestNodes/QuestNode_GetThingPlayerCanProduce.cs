767d97d4ee8703ef34ca8c47d3787f729f70de51
Taranchuk <nfhfyxer@gmail.com>
2021-01-29 14:38:30 +0200

    update

---
+++ 1.2/Source/FalloutRedScare/QuestNodes/QuestNode_GetThingPlayerCanProduce.cs
@@ -0,0 +1,108 @@
+﻿using RimWorld;
+using RimWorld.QuestGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace FalloutRedScare
+{
+    public class QuestNode_GetThingPlayerCanProduce : QuestNode
+    {
+        [NoTranslate]
+        public SlateRef<string> storeProductionItemDefAs;
+
+        [NoTranslate]
+        public SlateRef<string> storeProductionItemStuffAs;
+
+        [NoTranslate]
+        public SlateRef<string> storeProductionItemCountAs;
+
+        [NoTranslate]
+        public SlateRef<string> storeProductionItemLabelAs;
+
+        [NoTranslate]
+        public SlateRef<FloatRange> totalMarketValuePerPlayerWealth;
+
+        [NoTranslate]
+        public SlateRef<List<ThingDef>> thingDefCandidates;
+
+        [NoTranslate]
+        public SlateRef<List<ThingDef>> thingStuffCandidates;
+
+        private static List<Pair<ThingStuffPair, int>> tmpCandidates = new List<Pair<ThingStuffPair, int>>();
+
+        protected override bool TestRunInt(Slate slate)
+        {
+            return DoWork(slate);
+        }
+
+        protected override void RunInt()
+        {
+            DoWork(QuestGen.slate);
+        }
+
+        private bool DoWork(Slate slate)
+        {
+            Map map = slate.Get<Map>("map");
+            if (map == null)
+            {
+                return false;
+            }
+
+            tmpCandidates.Clear();
+            var totalWealth = map.wealthWatcher.WealthTotal;
+            foreach (var candidate in thingDefCandidates.GetValue(slate))
+            {
+                var goalMarketValue = totalWealth * totalMarketValuePerPlayerWealth.GetValue(slate).RandomInRange;
+                var stuffCandidate = GetStuffFor(candidate, slate);
+                var thingMarketValue = StatWorker_MarketValue.CalculatedBaseMarketValue(candidate, stuffCandidate);
+                //var goalThingCount = (int)(goalMarketValue / thingMarketValue);
+                var goalThingCount = 1; // for easier testing
+                Log.Message($"candidate: {candidate} - totalWealth: {totalWealth} - goalMarketValue: {goalMarketValue} - goalThingCount: {goalThingCount} " +
+                    $"- stuffCandidate: {stuffCandidate} - thingMarketValue: {thingMarketValue}");
+                tmpCandidates.Add(new Pair<ThingStuffPair, int>(new ThingStuffPair(candidate, stuffCandidate), goalThingCount));
+            }
+            if (!tmpCandidates.Any())
+            {
+                return false;
+            }
+            Pair<ThingStuffPair, int> pair = tmpCandidates.RandomElement();
+            int num6 = pair.Second;
+            num6 = Mathf.Max(num6, 1);
+            slate.Set(storeProductionItemDefAs.GetValue(slate), pair.First.thing);
+            slate.Set(storeProductionItemStuffAs.GetValue(slate), pair.First.stuff);
+            slate.Set(storeProductionItemCountAs.GetValue(slate), num6);
+            string value3 = storeProductionItemLabelAs.GetValue(slate);
+            if (!string.IsNullOrEmpty(value3))
+            {
+                slate.Set(value3, GenLabel.ThingLabel(pair.First.thing, pair.First.stuff, num6));
+            }
+            return true;
+        }
+
+        public ThingDef GetStuffFor(ThingDef candidate, Slate slate)
+        {
+            var stuffCandidates = GenStuff.AllowedStuffsFor(candidate);
+            if (stuffCandidates.Any())
+            {
+                if (thingStuffCandidates.GetValue(slate) != null)
+                {
+                    stuffCandidates = stuffCandidates.Where(x => thingStuffCandidates.GetValue(slate).Contains(x));
+                    if (stuffCandidates.Any())
+                    {
+                        return stuffCandidates.RandomElement();
+                    }
+                }
+                else
+                {
+                    return stuffCandidates.RandomElement();
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ 1.2/Source/FalloutRedScare/QuestNodes/QuestNode_SetRewards.cs
@@ -0,0 +1,55 @@
+﻿using RimWorld;
+using RimWorld.QuestGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace FalloutRedScare
+{
+    public class QuestNode_SetRewards : QuestNode
+    {
+		public SlateRef<IntRange> goodwillRange;
+		public SlateRef<IntRange> favorRange;
+
+		public SlateRef<Pawn> giveTo;
+		public SlateRef<string> inSignal;
+		protected override bool TestRunInt(Slate slate)
+        {
+            return true;
+        }
+
+        protected override void RunInt()
+        {
+			Slate slate = QuestGen.slate;
+
+			QuestPart_Choice questPart_Choice = new QuestPart_Choice();
+			QuestPart_Choice.Choice choice = new QuestPart_Choice.Choice();
+			Reward_Goodwill reward_Goodwill = new Reward_Goodwill();
+			reward_Goodwill.faction = slate.Get<Faction>("askerFaction");
+			reward_Goodwill.amount = goodwillRange.GetValue(slate).RandomInRange;
+			choice.rewards.Add(reward_Goodwill);
+			questPart_Choice.choices.Add(choice);
+
+			QuestPart_GiveRoyalFavor questPart_GiveRoyalFavor = new QuestPart_GiveRoyalFavor();
+			questPart_GiveRoyalFavor.giveTo = giveTo.GetValue(slate);
+			questPart_GiveRoyalFavor.giveToAccepter = true;
+			questPart_GiveRoyalFavor.faction = slate.Get<Faction>("askerFaction");
+			questPart_GiveRoyalFavor.amount = favorRange.GetValue(slate).RandomInRange;
+			questPart_GiveRoyalFavor.inSignal = (QuestGenUtility.HardcodedSignalWithQuestID(inSignal.GetValue(slate)) ?? QuestGen.slate.Get<string>("inSignal"));
+			QuestGen.quest.AddPart(questPart_GiveRoyalFavor);
+
+			QuestPart_Choice.Choice choice2 = new QuestPart_Choice.Choice();
+			Reward_RoyalFavor reward_Favor = new Reward_RoyalFavor();
+			reward_Favor.faction = slate.Get<Faction>("askerFaction");
+			reward_Favor.amount = questPart_GiveRoyalFavor.amount;
+			choice2.rewards.Add(reward_Favor);
+			questPart_Choice.choices.Add(choice2);
+
+			QuestGen.quest.AddPart(questPart_Choice);
+		}
+    }
+}
