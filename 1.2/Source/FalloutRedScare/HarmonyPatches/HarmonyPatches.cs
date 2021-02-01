3dca6045199f60ee57be1c4cad2f98b538b0f3cf
Виталий Буланенков <v.bulanenkov@corp.nekki.ru>
2021-02-01 20:45:30 +0300

    ain't the most efficient code for total war like mechanics, but it seems to work

--- 1.2/Source/FalloutRedScare/HarmonyPatches/HarmonyPatches.cs
+++ 1.2/Source/FalloutRedScare/HarmonyPatches/HarmonyPatches.cs
@@ -14,6 +14,7 @@ namespace FalloutRedScare
         static HarmonyInit()
         {
             new Harmony("Chismar.FalloutRedScare").PatchAll();
+            
         }
     }
 
---
+++ 1.2/Source/FalloutRedScare/PawnGroupMakerPR.cs
@@ -0,0 +1,24 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace FalloutRedScare
+{
+    public class PawnGroupMakerPR : PawnGroupMaker
+    {
+        public int minimumPower = 0;
+        public int maximumPower = int.MaxValue;
+
+        public bool CanGenerate(PawnGroupMakerParms parms)
+        {
+            var wctw = Find.World.GetComponent<WorldComponent_TotalWar>();
+            if (!wctw.factions.TryGetValue(parms.faction, out var fw))
+                return true;
+            return fw.points >= minimumPower && fw.points < maximumPower;
+        }
+    }
+}
