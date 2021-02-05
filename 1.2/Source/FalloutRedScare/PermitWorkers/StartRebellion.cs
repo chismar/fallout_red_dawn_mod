894dfe12dccad3e94ade11c73314d99909aae622
Taranchuk <nfhfyxer@gmail.com>
2021-02-05 20:51:54 +0200

    update

---
+++ 1.2/Source/FalloutRedScare/PermitWorkers/StartRebellion.cs
@@ -0,0 +1,66 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using Verse.AI.Group;
+
+namespace FalloutRedScare
+{
+	public class RebellionSettings
+	{
+		public string rebellionCommandKey;
+		public string rebellionNoPrisonersAvailableKey;
+		public List<ThingDef> weaponsToGivePrisoners;
+	}
+
+	public class StartRebellion : FRS_ScriptedTitlePermitWorker<RebellionSettings>
+	{
+		public override IEnumerable<FloatMenuOption> GetRoyalAidOptions(Map map, Pawn pawn, Faction faction)
+		{
+			if (AidDisabled(map, pawn, faction, out string reason))
+			{
+				yield return new FloatMenuOption(def.LabelCap + ": " + reason, null);
+				yield break;
+			}
+			if (!pawn.Map.mapPawns.AllPawns.Where(x => x.IsPrisoner).Any())
+            {
+				yield return new FloatMenuOption(def.LabelCap + ": " + workerSettings.rebellionNoPrisonersAvailableKey.Translate(), null);
+				yield break;
+			}
+			Action action = null;
+			string description = def.LabelCap + " (" + workerSettings.rebellionCommandKey.Translate() + "): ";
+			if (FillAidOption(pawn, faction, ref description, out bool free))
+			{
+				action = delegate
+				{
+					DoRebellion(pawn);
+				};
+			}
+			yield return new FloatMenuOption(description, action, faction.def.FactionIcon, faction.Color);
+		}
+		private void DoRebellion(Pawn caster)
+		{
+			var prisoners = caster.Map.mapPawns.AllPawns.Where(x => x.IsPrisoner);
+			foreach (var prisoner in prisoners)
+            {
+				prisoner.SetFaction(caster.Faction);
+				var weaponDef = workerSettings.weaponsToGivePrisoners.RandomElement();
+				var weapon = ThingMaker.MakeThing(weaponDef);
+				prisoner.equipment.AddEquipment(weapon as ThingWithComps);
+            }
+			LordMaker.MakeNewLord(caster.Faction, MakeLordJob(prisoners.RandomElement().Position, caster.Faction, caster.Map), caster.Map, prisoners);
+		}
+
+		protected LordJob MakeLordJob(IntVec3 cell, Faction faction, Map map)
+		{
+			if (faction.HostileTo(Faction.OfPlayer))
+			{
+				return new LordJob_AssaultColony(faction);
+			}
+			return new LordJob_AssistColony(faction, cell);
+		}
+	}
+}
---
+++ 1.2/Source/FalloutRedScare/PermitWorkers/SwitchSides.cs
@@ -0,0 +1,71 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using Verse.AI.Group;
+
+namespace FalloutRedScare
+{
+	public class SwitchSidesSettings
+	{
+		public string switchSidesCommandKey;
+		public float radiusToSwitch;
+		public ThoughtDef giveThoughtToPlayerPawns;
+	}
+	public class SwitchSides : FRS_ScriptedTitlePermitWorker<SwitchSidesSettings>
+	{
+		public static TargetingParameters ForLoc()
+		{
+			TargetingParameters targetingParameters = new TargetingParameters();
+			targetingParameters.canTargetLocations = true;
+			return targetingParameters;
+		}
+		public override IEnumerable<FloatMenuOption> GetRoyalAidOptions(Map map, Pawn pawn, Faction faction)
+		{
+			if (AidDisabled(map, pawn, faction, out string reason))
+			{
+				yield return new FloatMenuOption(def.LabelCap + ": " + reason, null);
+				yield break;
+			}
+			Action action = null;
+			string description = def.LabelCap + " (" + workerSettings.switchSidesCommandKey.Translate() + "): ";
+			if (FillAidOption(pawn, faction, ref description, out bool free))
+			{
+				action = delegate
+				{
+					Find.Targeter.BeginTargeting(ForLoc(), delegate (LocalTargetInfo x)
+					{
+						DoSwitchSides(pawn, x.Cell);
+					}, (LocalTargetInfo x) => GenDraw.DrawRadiusRing(x.Cell, workerSettings.radiusToSwitch), null, null);
+				};
+			}
+			yield return new FloatMenuOption(description, action, faction.def.FactionIcon, faction.Color);
+		}
+		private void DoSwitchSides(Pawn caster, IntVec3 cell)
+		{
+			var pawnsToSwitchSide = caster.Map.mapPawns.AllPawns.Where(x => x.RaceProps.Humanlike && 
+				x.Faction != caster.Faction && x.Position.DistanceTo(cell) <= workerSettings.radiusToSwitch).ToList();
+			if (caster.Faction != Faction.OfPlayer)
+            {
+				var playerPawns = pawnsToSwitchSide.Where(x => x.Faction == Faction.OfPlayer);
+				pawnsToSwitchSide.RemoveAll(x => playerPawns.Contains(x));
+				foreach (var pawn in playerPawns)
+                {
+					pawn.needs.mood.thoughts.memories.TryGainMemory(workerSettings.giveThoughtToPlayerPawns);
+                }
+            }
+			var lord = caster.GetLord();
+			foreach (var pawn in pawnsToSwitchSide)
+            {
+				pawn.SetFaction(caster.Faction);
+				if (lord != null)
+                {
+					lord.AddPawn(pawn);
+                }
+            }
+		}
+	}
+}
