5dbade97f9d1ac58b75d0f632213c63cb0ae414e
Виталий Буланенков <v.bulanenkov@corp.nekki.ru>
2021-02-07 01:52:45 +0300

    working bombing run

---
+++ 1.2/Source/FalloutRedScare/Comps/ShuttleBomber.cs
@@ -0,0 +1,67 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using RimWorld.QuestGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+using Verse.Sound;
+
+namespace FalloutRedScare
+{
+	/*public class CompProperties_ShuttleBomber : CompProperties
+    {
+		public CompProperties_ShuttleBomber()
+        {
+			this.compClass = typeof(CompShuttleBomber);
+        }
+	}*/
+	public class ShuttleBomber : Skyfaller
+	{
+		public List<ThingDef> shells;
+
+		public Map targetMap;
+        public override Vector3 DrawPos => SkyfallerDrawPosUtility.DrawPos_ConstantSpeed(this.TrueCenter(), this.ticksToImpact, this.angle, this.def.skyfaller.speed);
+        public IntVec3 GetPos()
+        {
+            return DrawPos.ToIntVec3();
+        }
+        bool reversed;
+        public override void Tick()
+        {
+            if(this.ticksToImpact == 1)
+            {
+                reversed = true;
+                this.angle = angle+180;
+            }
+            if (reversed)
+                ticksToImpact++;
+            if (ticksToImpact == 220)
+                ticksToImpact = 0;
+                base.Tick();
+            if(reversed && ticksToImpact != 0)
+                ticksToImpact++;
+            
+            if (Find.TickManager.TicksGame % 20 == 0)
+			{
+				var curCell = GetPos();
+				if (curCell.InBounds(targetMap))
+                {
+                    Projectile projectile = (Projectile)GenSpawn.Spawn(shells.RandomElement(), curCell, targetMap, WipeMode.Vanish);
+                    projectile.Launch(null, curCell.ToVector3ShiftedWithAltitude(AltitudeLayer.Projectile), curCell, curCell, ProjectileHitFlags.All, null, null);
+                }
+                
+			}
+		}
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Collections.Look(ref shells, "shells", LookMode.Def);
+			Scribe_References.Look(ref targetMap, "targetMap");
+        }
+    }
+}
--- 1.2/Source/FalloutRedScare/PermitWorkers/AirBombing.cs
+++ 1.2/Source/FalloutRedScare/PermitWorkers/AirBombing.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 using Verse;
 using Verse.AI.Group;
 
@@ -34,17 +35,21 @@ namespace FalloutRedScare
 				{
 					Find.Targeter.BeginTargeting(ForLoc(), delegate (LocalTargetInfo x)
 					{
-						var shuttle = ThingMaker.MakeThing(workerSettings.shuttleBomberDef, null);
-						var comp = shuttle.TryGetComp<CompShuttle>();
-						var compTransporter = ThingCompUtility.TryGetComp<CompTransporter>(shuttle);
-						var startingCell = CellRect.WholeMap(map).EdgeCells.OrderBy(cell => cell.DistanceTo(x.Cell)).FirstOrDefault();
-						GenPlace.TryPlaceThing(SkyfallerMaker.MakeSkyfaller(workerSettings.skyfallerBomber, shuttle), startingCell, map, ThingPlaceMode.Near, null, null, default(Rot4));
-						comp.requiredColonistCount = 0;
-						comp.missionShuttleTarget = map.Parent;
-						comp.missionShuttleHome = null;
-						var compBomber = shuttle.TryGetComp<CompShuttleBomber>();
+						//var shuttle = ThingMaker.MakeThing(workerSettings.shuttleBomberDef, null);
+
+						//var comp = shuttle.TryGetComp<CompShuttle>();
+						// compTransporter = ThingCompUtility.TryGetComp<CompTransporter>(shuttle);
+						//var startingCell = CellRect.WholeMap(map).EdgeCells.OrderBy(cell => cell.DistanceTo(x.Cell)).FirstOrDefault();
+						var sk = SkyfallerMaker.MakeSkyfaller(workerSettings.skyfallerBomber);
+						GenPlace.TryPlaceThing(sk, x.Cell, map, ThingPlaceMode.Near, null, null, default(Rot4));
+						//comp.requiredColonistCount = 0;
+						//comp.missionShuttleTarget = map.Parent;
+						//comp.missionShuttleHome = null;
+						var compBomber = sk as ShuttleBomber;
 						compBomber.shells = workerSettings.shells;
 						compBomber.targetMap = map;
+						var dir = (x.Cell - pawn.Position).ToVector3();
+						compBomber.angle = Vector3.SignedAngle(dir, Vector3.forward, Vector3.up);
 					}, null, null);
 				};
 			}
