72ec2e7beb25178055069860e540b20ddc8da043
Виталий Буланенков <v.bulanenkov@corp.nekki.ru>
2021-02-10 20:30:13 +0300

    basic items for red army

---
+++ 1.2/Source/FalloutRedScare/ApparelComp_RestrictBodyType.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace FalloutRedScare
+{
+    class ApparelComp_RestrictBodyType : ThingComp
+    {
+    }
+    public class CompProperties_ApparelComp_RestrictBodyType : CompProperties
+    {
+        public List<BodyTypeDef> restrictBodyTypes;
+        public CompProperties_ApparelComp_RestrictBodyType()
+        {
+            this.compClass = typeof(ApparelComp_RestrictBodyType);
+        }
+    }
+}
---
+++ 1.2/Source/FalloutRedScare/HarmonyPatches/Apparel_Patches.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+
+namespace FalloutRedScare
+{
+
+    internal static class Apparel_Patches
+    {
+        [HarmonyPatch(typeof(ApparelUtility), nameof(ApparelUtility.HasPartsToWear))]
+        private static class FactionForCombatGroup_Patch
+        {
+            public static bool Prefix(Pawn __0, ThingDef __1, ref bool __result)
+            {
+                for(int i =0; i < __1.comps.Count; i++)
+                {
+                    if(__1.comps[i] is CompProperties_ApparelComp_RestrictBodyType restrict)
+                    {
+                        if(restrict.restrictBodyTypes != null)
+                        for (int j = 0; j < restrict.restrictBodyTypes.Count;j++)
+                        {
+                            if(__0.story.bodyType == restrict.restrictBodyTypes[j])
+                                {
+                                    __result = false;
+                                    return false;
+                                }
+                        }
+                    }
+                }
+                return true;
+            }
+        }
+
+    }
+}
