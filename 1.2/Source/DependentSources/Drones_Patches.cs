215cb0f5c6ee7f147f95db900a5a75b47c94ae64
Виталий Буланенков <v.bulanenkov@corp.nekki.ru>
2021-02-19 18:47:53 +0300

    fix drones spawning in ancient danger

--- 1.2/Source/DependentSources/Drones_Patches.cs
+++ 1.2/Source/DependentSources/Drones_Patches.cs
@@ -1,9 +1,11 @@
 ﻿using HarmonyLib;
 using RimWorld;
+using RimWorld.BaseGen;
 using RimWorld.Planet;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Verse;
@@ -14,6 +16,26 @@ using VFEMech;
 
 namespace FalloutRedScare
 {
+    [HarmonyPatch]
+    public class MechSpawn_Patch
+    {
+        [HarmonyTargetMethods]
+        public static IEnumerable<MethodBase> TargetMethods()
+        {
+            yield return typeof(SymbolResolver_RandomMechanoidGroup).GetNestedTypes(AccessTools.all)
+                                                                 .First(t => t.GetMethods(AccessTools.all)
+                                                                           .Any(mi => mi.ReturnType == typeof(bool) && mi.GetParameters()[0].ParameterType == typeof(PawnKindDef)))
+                                                                 .GetMethods(AccessTools.all)
+                                                                 .First(mi => mi.ReturnType == typeof(bool));
+
+            yield return typeof(MechClusterGenerator).GetNestedTypes(AccessTools.all).MaxBy(t => t.GetMethods(AccessTools.all).Length).GetMethods(AccessTools.all)
+                                                  .First(mi => mi.ReturnType == typeof(bool) && mi.GetParameters()[0].ParameterType == typeof(PawnKindDef));
+        }
+
+        [HarmonyPostfix]
+        public static void Postfix(PawnKindDef __0, ref bool __result) =>
+            __result &= !__0.race.defName.StartsWith("FRS");
+    }
     [HarmonyPatch(typeof(Pawn), nameof(Pawn.ExitMap))]
     public static class ForbidPlayerPawnsFromDissapearingIntoTheAbyss
     {
---
+++ 1.2/Source/DependentSources/HarmonyPatches.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using RimWorld;
+using RimWorld.Planet;
+using RimWorld.QuestGen;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace FalloutRedScare
+{
+    [StaticConstructorOnStartup]
+    internal static class HarmonyInit
+    {
+        static HarmonyInit()
+        {
+            new Harmony("Chismar.FalloutRedScare.DependentCode").PatchAll();
+            
+        }
+    }
+}
