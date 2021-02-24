8d418558b477edbbac9607c2a2913b73dd1c3051
Виталий Буланенков <v.bulanenkov@corp.nekki.ru>
2021-02-25 00:47:10 +0300

    mostly independence from vanilla lasers

---
+++ 1.2/Source/FalloutRedScare/Lasers/LaserRay.cs
@@ -0,0 +1,135 @@
+﻿
+using System;
+using RimWorld;
+using UnityEngine;
+using Verse;
+namespace RedScare.Lasers
+{
+    internal interface IDrawnWeaponWithRotation
+    {
+        float RotationOffset { get; set; }
+    }
+    public static class ThingExtensions
+    {
+        // Token: 0x0600005F RID: 95 RVA: 0x0000440C File Offset: 0x0000260C
+        public static bool IsShielded(this Thing thing)
+        {
+            return (thing as Pawn).IsShielded();
+        }
+
+        // Token: 0x06000060 RID: 96 RVA: 0x0000442C File Offset: 0x0000262C
+        public static bool IsShielded(this Pawn pawn)
+        {
+            bool flag = pawn == null || pawn.apparel == null;
+            bool result;
+            if (flag)
+            {
+                result = false;
+            }
+            else
+            {
+                DamageInfo damageInfo = new DamageInfo(DamageDefOf.Bomb, 0f, 0f, -1f, null, null, null, 0, null);
+                foreach (Apparel apparel in pawn.apparel.WornApparel)
+                {
+                    bool flag2 = apparel.CheckPreAbsorbDamage(damageInfo);
+                    if (flag2)
+                    {
+                        return true;
+                    }
+                }
+                result = false;
+            }
+            return result;
+        }
+    }
+    public class LaserRay : Bullet
+    {
+        private LaserRayDef Def => base.def as LaserRayDef;
+
+        public override void Draw()
+        {
+        }
+
+        private void TriggerEffect(EffecterDef effect, Vector3 position)
+        {
+            this.TriggerEffect(effect, IntVec3.FromVector3(position));
+        }
+
+        private void TriggerEffect(EffecterDef effect, IntVec3 dest)
+        {
+            bool flag = effect == null;
+            if (!flag)
+            {
+                var targetInfo = new TargetInfo(dest, base.Map, false);
+                Effecter effecter = effect.Spawn();
+                effecter.Trigger(targetInfo, targetInfo);
+                effecter.Cleanup();
+            }
+        }
+
+        private void SpawnBeam(Vector3 a, Vector3 b)
+        {
+            var laserBeamGraphic = ThingMaker.MakeThing(this.Def.beamGraphic, null) as LaserRayGraphic;
+            bool flag = laserBeamGraphic == null;
+            if (!flag)
+            {
+                laserBeamGraphic.def = this.Def;
+                laserBeamGraphic.Setup(this.launcher, a, b);
+                GenSpawn.Spawn(laserBeamGraphic, IntVec3Utility.ToIntVec3(this.origin), base.Map, 0);
+            }
+        }
+
+        // Token: 0x06000039 RID: 57 RVA: 0x00002E0C File Offset: 0x0000100C
+        private void SpawnBeamReflections(Vector3 a, Vector3 b, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Vector3 normalized = (b - a).normalized;
+                Vector3 b2 = b - Vector3Utility.RotatedBy(normalized, Rand.Range(-22.5f, 22.5f)) * Rand.Range(1f, 4f);
+                this.SpawnBeam(b, b2);
+            }
+        }
+
+        // Token: 0x0600003A RID: 58 RVA: 0x00002E78 File Offset: 0x00001078
+        protected override void Impact(Thing hitThing)
+        {
+            var isShielded = hitThing.IsShielded();
+            var laserGunDef = this.equipmentDef as LaserRayGunDef;
+            Vector3 normalized = (this.destination - this.origin).normalized;
+            normalized.y = 0f;
+            Vector3 vector = this.origin + normalized * ((laserGunDef == null) ? 0.9f : laserGunDef.barrelLength);
+            Vector3 vector2 = isShielded ? (GenThing.TrueCenter(hitThing) - Vector3Utility.RotatedBy(normalized, Rand.Range(-22.5f, 22.5f)) * 0.8f) : this.destination;
+            vector.y = (vector2.y = this.Def.Altitude);
+            this.SpawnBeam(vector, vector2);
+
+            Pawn pawn = this.launcher as Pawn;
+            IDrawnWeaponWithRotation drawnWeaponWithRotation = null;
+            bool flag4 = pawn != null && pawn.equipment != null;
+            if (flag4)
+            {
+                drawnWeaponWithRotation = (pawn.equipment.Primary as IDrawnWeaponWithRotation);
+            }
+            if (drawnWeaponWithRotation != null)
+            {
+                float num = Vector3Utility.AngleFlat(vector2 - vector) - Vector3Utility.AngleFlat(this.intendedTarget.CenterVector3 - vector);
+                drawnWeaponWithRotation.RotationOffset = (num + 180f) % 360f - 180f;
+            }
+            if (hitThing == null)
+            {
+                this.TriggerEffect(this.Def.explosionEffect, this.destination);
+            }
+            else
+            {
+                if (hitThing is Pawn && isShielded)
+                {
+                    this.weaponDamageMultiplier *= this.Def.shieldDamageMultiplier;
+                    this.SpawnBeamReflections(vector, vector2, 5);
+                }
+                this.TriggerEffect(this.Def.explosionEffect, this.ExactPosition);
+            }
+            base.Impact(hitThing);
+        }
+    }
+
+
+}
---
+++ 1.2/Source/FalloutRedScare/Lasers/LaserRayGraphic.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace RedScare.Lasers
+{
+	public class LaserRayGraphic : Thing
+	{
+
+		private int ticks;
+		private Vector3 a;
+		private Vector3 b;
+		public Matrix4x4 drawingMatrix = default(Matrix4x4);
+		private Material materialBeam;
+		private Mesh mesh;
+		private LaserRayDef Def => base.def as LaserRayDef;
+
+
+		public float Opacity => (float)Math.Sin(Math.Pow(1.0 - 1.0 * (double)this.ticks / (double)this.Def.lifetime, (double)this.Def.impulse) * Mathf.PI);
+
+		public override void ExposeData()
+		{
+			base.ExposeData();
+			Scribe_Values.Look(ref this.ticks, "ticks", 0, false);
+			Scribe_Values.Look(ref this.a, "a", default, false);
+			Scribe_Values.Look(ref this.b, "b", default, false);
+		}
+
+		public override void Tick()
+		{
+			bool flag;
+			if (this.Def != null)
+			{
+				int num = this.ticks;
+				this.ticks = num + 1;
+				flag = (num > this.Def.lifetime);
+			}
+			else
+			{
+				flag = true;
+			}
+			bool flag2 = flag;
+			if (flag2)
+			{
+				this.Destroy(0);
+			}
+		}
+
+		
+		public void Setup(Thing launcher, Vector3 origin, Vector3 destination)
+		{
+			this.a = origin;
+			this.b = destination;
+		}
+
+		public void SetupDrawing()
+		{
+			bool flag = this.mesh != null;
+			if (!flag)
+			{
+				this.materialBeam = this.Def.graphicData.Graphic.MatSingle;
+				float beamWidth = this.Def.beamWidth;
+				Quaternion q = Quaternion.LookRotation(this.b - this.a);
+				float magnitude = (this.b - this.a).magnitude;
+				Vector3 s = new Vector3(beamWidth, 1f, magnitude);
+				Vector3 pos = (this.a + this.b) / 2f;
+				this.drawingMatrix.SetTRS(pos, q, s);
+				float num = 1f * (float)this.materialBeam.mainTexture.width / (float)this.materialBeam.mainTexture.height;
+				float num2 = (this.Def.seam < 0f) ? num : this.Def.seam;
+				float num3 = beamWidth / num / 2f * num2;
+				float sv = (magnitude <= num3 * 2f) ? 0.5f : (num3 * 2f / magnitude);
+				this.mesh = LaserRayMaker.Mesh(num2, sv);
+			}
+		}
+
+		public override void SpawnSetup(Map map, bool respawningAfterLoad)
+		{
+			base.SpawnSetup(map, respawningAfterLoad);
+			bool flag = this.Def == null || this.Def.decorations == null || respawningAfterLoad;
+			if (!flag)
+			{
+				foreach (var laserBeamDecoration in this.Def.decorations)
+				{
+					float num = laserBeamDecoration.spacing * this.Def.beamWidth;
+					float d = laserBeamDecoration.initialOffset * this.Def.beamWidth;
+					Vector3 normalized = (this.b - this.a).normalized;
+					float num2 = Vector3Utility.AngleFlat(this.b - this.a);
+					Vector3 vector = normalized * num;
+					Vector3 exactPosition = this.a + vector * 0.5f + normalized * d;
+					float num3 = (this.b - this.a).magnitude - num;
+					int num4 = 0;
+					while (num3 > 0f)
+					{
+						var moteLaserDectoration = ThingMaker.MakeThing(laserBeamDecoration.mote, null) as LaserMote;
+						bool flag2 = moteLaserDectoration == null;
+						if (flag2)
+						{
+							break;
+						}
+						moteLaserDectoration.ray = this;
+						moteLaserDectoration.airTimeLeft = (float)this.Def.lifetime;
+						moteLaserDectoration.Scale = this.Def.beamWidth;
+						moteLaserDectoration.exactRotation = num2;
+						moteLaserDectoration.exactPosition = exactPosition;
+						moteLaserDectoration.SetVelocity(num2, laserBeamDecoration.speed);
+						moteLaserDectoration.baseSpeed = laserBeamDecoration.speed;
+						moteLaserDectoration.speedJitter = laserBeamDecoration.speedJitter;
+						moteLaserDectoration.speedJitterOffset = laserBeamDecoration.speedJitterOffset * (float)num4;
+						GenSpawn.Spawn(moteLaserDectoration, IntVec3Utility.ToIntVec3(this.a), map, 0);
+						exactPosition += vector;
+						num3 -= num;
+						num4++;
+					}
+				}
+			}
+		}
+
+		public override void Draw()
+		{
+			this.SetupDrawing();
+			float opacity = this.Opacity;
+			bool flag = this.Def.graphicData.graphicClass == typeof(Graphic_Flicker);
+			if (flag)
+			{
+				bool flag2 = !Find.TickManager.Paused && Find.TickManager.TicksGame % this.Def.flickerFrameTime == 0;
+				if (flag2)
+				{
+					this.materialBeam = this.Def.graphicData.Graphic.MatSingle;
+				}
+			}
+			Graphics.DrawMesh(this.mesh, this.drawingMatrix, FadedMaterialPool.FadedVersionOf(this.materialBeam, opacity), 0);
+		}
+
+	}
+}
