4585b4c389c488c1929f403fc732a07ded40d2e4
Виталий Буланенков <v.bulanenkov@corp.nekki.ru>
2021-03-07 21:24:28 +0300

    production quota quest

--- 1.2/Source/FalloutRedScare/QuestNodes/QuestNode_GenerateSpecifiedShuttle.cs
+++ 1.2/Source/FalloutRedScare/QuestNodes/QuestNode_GenerateSpecifiedShuttle.cs
@@ -11,15 +11,26 @@ using Verse;
 namespace RedScare
 {
     public class QuestNode_GenerateSpecifiedShuttle : QuestNode_GenerateShuttle
-	{
-		public SlateRef<ThingDef> shuttle;
+    {
+        public SlateRef<ThingDef> shuttle;
+        public SlateRef<ThingDef> requiredDef;
+        public SlateRef<ThingDef> requiredStuff;
+        public SlateRef<int> requiredCount;
 
-		protected override void RunInt()
-		{
-			var prevS = ThingDefOf.Shuttle;
-			ThingDefOf.Shuttle = shuttle.GetValue(QuestGen.slate);
-			base.RunInt();
-			ThingDefOf.Shuttle = prevS;
-		}
-	}
+        protected override void RunInt()
+        {
+            var prevS = ThingDefOf.Shuttle;
+            ThingDefOf.Shuttle = shuttle.GetValue(QuestGen.slate);
+            base.RunInt();
+            ThingDefOf.Shuttle = prevS;
+            Slate slate = QuestGen.slate;
+            if (requiredDef.GetValue(slate) != null)
+            {
+                var thing = QuestGen.slate.Get<Thing>(this.storeAs.GetValue(slate));
+                var cs = thing.TryGetComp<CompShuttle>();
+                cs.requiredItems = new List<ThingDefCount>();
+                cs.requiredItems.Add(new ThingDefCount(requiredDef.GetValue(slate), requiredCount.GetValue(slate)));
+            }
+        }
+    }
 }
