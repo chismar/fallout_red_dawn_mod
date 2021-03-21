796f0951f0125e88e97739f36b8a2239e9008415
Виталий Буланенков <v.bulanenkov@corp.nekki.ru>
2021-03-21 23:15:41 +0300

    radial effect verb

--- 1.2/Source/FalloutRedScare/Comps/RadialEffect.cs
+++ 1.2/Source/FalloutRedScare/Comps/RadialEffect.cs
@@ -5,11 +5,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 using Verse;
 
 namespace RedScare
 {
-    internal static partial class CompTickPatch
+    /*internal static partial class CompTickPatch
     {
         [HarmonyPatch(typeof(Pawn_EquipmentTracker), nameof(Pawn_EquipmentTracker.EquipmentTrackerTick))]
         private static class CompTickPatch_Patch
@@ -24,55 +25,131 @@ namespace RedScare
             }
         }
 
-    }
-    public class CompProperties_RadialEffect : CompProperties
+    }*/
+    internal static partial class VerbsPatcher
     {
-        public CompProperties_RadialEffect()
+        [HarmonyPatch(typeof(Verb), nameof(Verb.VerbTick))]
+        private static class VerbsPatcher_Patch
         {
-            this.compClass = typeof(CompRadialEffect);
+            public static void Postfix(Verb __instance)
+            {
+                if (__instance is TickedVerb tv)
+                    tv.OnVerbTick();
+            }
         }
 
-        public int radius = 1;
+    }
+    internal static partial class TickedVerbsNotifyDespawnPatch
+    {
+        [HarmonyPatch(typeof(Pawn_EquipmentTracker), nameof(Pawn_EquipmentTracker.Notify_PawnDied))]
+        private static class VerbsPatcher_Patch
+        {
+            public static void Postfix(Pawn_EquipmentTracker __instance)
+            {
+                foreach (var verb in __instance.AllEquipmentVerbs)
+                    if (verb is TickedVerb tv)
+                        tv.NotifyPawnDied();
+            }
+        }
+
+    }
+    public class VerbProperties_RadialEffect : VerbProperties
+    {
+        public VerbProperties_RadialEffect()
+        {
+            this.verbClass = typeof(VerbRadialEffect);
+        }
+
         public float severity = 1f;
         public float tickInterval = 1000;
         public bool notOnlyAffectColonists = false;
         public HediffDef hediff;
     }
-    public class CompRadialEffect : ThingComp
+
+    public class TickedVerb : Verb
     {
-        public CompProperties_RadialEffect Props
+        public virtual void OnVerbTick()
+        {
+
+        }
+
+        protected override bool TryCastShot()
+        {
+            return false;
+        }
+
+        public virtual void NotifyPawnDied()
+        {
+        }
+    }
+    public class VerbRadialEffect : TickedVerb
+    {
+        public VerbProperties_RadialEffect Props
         {
             get
             {
-                return (CompProperties_RadialEffect)this.props;
+                return (VerbProperties_RadialEffect)this.verbProps;
             }
         }
-        public override void CompTick()
+        static List<Thing> _staticListForQuery = new List<Thing>();
+        static HashSet<Hediff> _addedThisTick = new HashSet<Hediff>();
+        Dictionary<Pawn, Hediff> _previousThings = new Dictionary<Pawn, Hediff>();
+        public override void OnVerbTick()
         {
-
-            Log.Message(string.Format("CompTickRare"), false);
-
             if (Find.TickManager.TicksGame - lastTick > Props.tickInterval * GenDate.TicksPerHour)
             {
                 lastTick = Find.TickManager.TicksGame;
-                var pawn = parent.ParentHolder.ParentHolder as Pawn;
-                Log.Message($"CompTickRare2 {parent?.GetType().Name} {parent?.ParentHolder?.GetType().Name} {parent?.ParentHolder?.ParentHolder?.GetType().Name}", false);
+                var pawn = this.CasterPawn;
                 if (pawn != null && pawn.Map != null)
                 {
-                    List<Pawn> list = this.Props.notOnlyAffectColonists ? pawn.Map.mapPawns.AllPawns : pawn.Map.mapPawns.FreeColonists;
-                    Log.Message($"CompTickRare3 {list.Count}", false);
-                    foreach (Pawn pawn2 in list)
+                    if (SpatialHashMapComp.HashMapPerMap.TryGetValue(pawn.Map, out var hash))
                     {
-                        bool flag3 = !pawn2.Dead && !pawn2.Downed && pawn.Position.InHorDistOf(pawn2.Position, (float)this.Props.radius);
-                        if (flag3)
+                        hash.GetAllAround(pawn.Position.ToIntVec2, Mathf.RoundToInt(this.Props.range), _staticListForQuery);
+                        foreach (var thing in _staticListForQuery)
                         {
-                            Log.Message(string.Format("Adding Boastforme hediff to: {0}", pawn2.Name), false);
-                            pawn2.health.AddHediff(Props.hediff, null, null, null);
+                            var pawn2 = thing as Pawn;
+                            if (pawn2 == null)
+                                continue;
+                            if (!pawn2.Dead && !pawn2.Downed)
+                            {
+                                if (!_previousThings.ContainsKey(pawn2))
+                                    _addedThisTick.Add(pawn2.health.AddHediff(Props.hediff, null, null, null));
+                                else
+                                {
+                                    _previousThings.Remove(pawn2, out var hediff);
+                                    _addedThisTick.Add(hediff);
+                                }
+                            }
                         }
                     }
                 }
+                foreach (var prevThing in _previousThings)
+                    prevThing.Key.health.RemoveHediff(prevThing.Value);
+                _previousThings.Clear();
+                foreach (var addedObject in _addedThisTick)
+                    _previousThings.Add(addedObject.pawn, addedObject);
+                _staticListForQuery.Clear();
+                _addedThisTick.Clear();
             }
         }
+
+        public override void NotifyPawnDied()
+        {
+            Notify_EquipmentLost();
+        }
+
+        public override void Notify_EquipmentLost()
+        {
+            foreach (var prevThing in _previousThings)
+                prevThing.Key.health.RemoveHediff(prevThing.Value);
+            _previousThings.Clear();
+        }
+
+        protected override bool TryCastShot()
+        {
+            return false;
+        }
+
         public int lastTick = 0;
     }
 }
