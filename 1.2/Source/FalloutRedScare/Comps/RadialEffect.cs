509f4a7a87cfcc18d81fbd35f1f06d5a8bb11d01
Виталий Буланенков <v.bulanenkov@corp.nekki.ru>
2021-03-24 23:30:09 +0300

    radial hediffs

--- 1.2/Source/FalloutRedScare/Comps/RadialEffect.cs
+++ 1.2/Source/FalloutRedScare/Comps/RadialEffect.cs
@@ -62,7 +62,8 @@ namespace RedScare
 
         public float severity = 1f;
         public float tickInterval = 1000;
-        public bool notOnlyAffectColonists = false;
+        public bool onlyAffectsSameFaction = false;
+        public FactionDef onlyAffectsFactionMembers = null;
         public HediffDef hediff;
     }
 
@@ -82,6 +83,33 @@ namespace RedScare
         {
         }
     }
+    public class NonSavedHediffDef : HediffDef
+    {
+
+    }
+    internal static partial class HediffsSetPatcher
+    {
+        [HarmonyPatch(typeof(HediffSet), nameof(HediffSet.ExposeData))]
+        private static class HediffsSetPatcherPatcher
+        {
+            static List<Hediff> _hediffsCache = new List<Hediff>();
+            public static void Prefix(HediffSet __instance)
+            {
+                _hediffsCache.Clear();
+                foreach (var hediff in __instance.hediffs)
+                    if (hediff.def is NonSavedHediffDef)
+                        _hediffsCache.Add(hediff);
+                __instance.hediffs.RemoveAll(x => x.def is NonSavedHediffDef);
+            }
+            public static void Postfix(HediffSet __instance)
+            {
+                foreach (var hediff in _hediffsCache)
+                    __instance.hediffs.Add(hediff);
+                _hediffsCache.Clear();
+            }
+        }
+
+    }
     public class VerbRadialEffect : TickedVerb
     {
         public VerbProperties_RadialEffect Props
@@ -94,6 +122,28 @@ namespace RedScare
         static List<Thing> _staticListForQuery = new List<Thing>();
         static HashSet<Hediff> _addedThisTick = new HashSet<Hediff>();
         Dictionary<Pawn, Hediff> _previousThings = new Dictionary<Pawn, Hediff>();
+        static Dictionary<Pawn, HashSet<HediffDef>> _addedHediffs = new Dictionary<Pawn, HashSet<HediffDef>>();
+
+        bool AlreadyHasHediff(Pawn targetPawn)
+        {
+            if (_addedHediffs.TryGetValue(targetPawn, out var set))
+            {
+                return set.Contains(Props.hediff) && !_previousThings.ContainsKey(targetPawn);
+            }
+            return false;
+        }
+        void AddToAddedHediffs(Pawn targetPawn)
+        {
+            if (!_addedHediffs.TryGetValue(targetPawn, out var set))
+                _addedHediffs.Add(targetPawn, set = new HashSet<HediffDef>());
+            set.Add(Props.hediff);
+        }
+
+        void RemoveFromAddedHediffs(Pawn targetPawn)
+        {
+            if (_addedHediffs.TryGetValue(targetPawn, out var set))
+                set.Remove(Props.hediff);
+        }
         public override void OnVerbTick()
         {
             if (Find.TickManager.TicksGame - lastTick > Props.tickInterval * GenDate.TicksPerHour)
@@ -110,10 +160,13 @@ namespace RedScare
                             var pawn2 = thing as Pawn;
                             if (pawn2 == null)
                                 continue;
-                            if (!pawn2.Dead && !pawn2.Downed)
+                            if (!pawn2.Dead && !pawn2.Downed && (!Props.onlyAffectsSameFaction || pawn2.Faction == pawn.Faction) && !AlreadyHasHediff(pawn2))
                             {
                                 if (!_previousThings.ContainsKey(pawn2))
+                                {
+                                    AddToAddedHediffs(pawn2);
                                     _addedThisTick.Add(pawn2.health.AddHediff(Props.hediff, null, null, null));
+                                }
                                 else
                                 {
                                     _previousThings.Remove(pawn2, out var hediff);
@@ -124,7 +177,10 @@ namespace RedScare
                     }
                 }
                 foreach (var prevThing in _previousThings)
+                {
+                    RemoveFromAddedHediffs(prevThing.Key);
                     prevThing.Key.health.RemoveHediff(prevThing.Value);
+                }
                 _previousThings.Clear();
                 foreach (var addedObject in _addedThisTick)
                     _previousThings.Add(addedObject.pawn, addedObject);
